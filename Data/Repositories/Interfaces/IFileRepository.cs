802f4ebbd75090e6f9fb290365968f08443f78e3
Andreas <andreas_fagerberg@outlook.com>
2025-06-03 10:54:21 +0200

    feat: Add FileRepository and updated FolderRepository

---
+++ Data/Repositories/Interfaces/IFileRepository.cs
@@ -0,0 +1,10 @@
+using Google_Drive_clone.Models.Entities;
+
+public interface IFileRepository
+{
+    public Task<FileEntity> CreateFileAsync(FileEntity fileEntity);
+    public Task<FileEntity> GetFileAsync(int fileId);
+    public Task<List<FileEntity>> GetAllFilesAsync();
+    public Task<FileEntity> UpdateFileAsync();
+    public Task DeleteFileAsync();
+}
